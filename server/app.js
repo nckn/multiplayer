e04f11e8f034a0c9e6ffe5391ababa8810bb9c39
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-05-06 10:12:39 +0200

    Trying to get hook working when a new client joins

--- server/app.js
+++ server/app.js
@@ -15,6 +15,47 @@ var position = {
   y: 100
 }
 
+var id = 0
+
+// Assigning IDs
+// socketio.engine.generateId = function (req) {
+//   // generate a new custom id here
+//   id++
+//   return id
+// }
+
+// var util = require("util"),
+//   io = require('/socket.io').listen(8080),
+//   fs = require('fs'),
+//   os = require('os'),
+//   url = require('url');
+
+var clients =[];
+
+// io.sockets.on('connection', function (socket) {
+
+//     socket.on('storeClientInfo', function (data) {
+
+//         var clientInfo = new Object();
+//         clientInfo.customId         = data.customId;
+//         clientInfo.clientId     = socket.id;
+//         clients.push(clientInfo);
+//     });
+
+//     socket.on('disconnect', function (data) {
+
+//         for( var i=0, len=clients.length; i<len; ++i ){
+//             var c = clients[i];
+
+//             if(c.clientId == socket.id){
+//                 clients.splice(i,1);
+//                 break;
+//             }
+//         }
+
+//     });
+// });
+
 // var clients = [];
 
 // socketio.sockets.on('connect', function(client) {
@@ -26,7 +67,11 @@ var position = {
 // });
 
 socketio.on('connection', socket => {
+  // Log ID
+  // console.log('new id: ', socket.id); // writes 1 on the console
+  // Set position
   socket.emit('position', position)
+  // When move is detected
   socket.on('move', data => {
     // Mouse movement
     position.x = data.x
@@ -39,6 +84,29 @@ socketio.on('connection', socket => {
     socketio.emit('getCount', total)
     
   })
+
+  // Store client info
+  socket.on('storeClientInfo', function (data) {
+    var clientInfo = new Object();
+    clientInfo.customId = data.customId;
+    clientInfo.clientId = socket.id;
+    // console.log('client Info: ', clientInfo)
+    clients.push(clientInfo);
+    // Store client info
+    socketio.emit('storeInfo', clientInfo)
+    return clientInfo
+  });
+
+  socket.on('disconnect', function (data) {
+    for (var i = 0, len = clients.length; i < len; ++i ){
+      var c = clients[i];
+      if (c.clientId == socket.id) {
+        clients.splice(i, 1);
+        break;
+      }
+    }
+  });
+
 })
 
 http.listen(3000, () => {
